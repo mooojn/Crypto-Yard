ea1c15463ad3657964210af5682917abbfc638ed
mooojn <moontariq7867@gmail.com>
2024-03-30 17:05:53 +0500

    err page

--- main-react/src/App.jsx
+++ main-react/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Root from './components/Root';
 import Loan from './components/Loan';
+import ErrorPage from './components/ErrorPage';
 
 function App() {
   return (
     <>
       {/* <Root /> */}
       {/* <Loan /> */}
+      {/* <ErrorPage /> */}
     </>
   )
 }
