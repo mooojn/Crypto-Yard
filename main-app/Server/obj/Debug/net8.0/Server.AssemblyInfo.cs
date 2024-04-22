76efeff794202d5f8ed7462638529886e8e33797
mooojn <moontariq7867@gmail.com>
2024-04-22 19:02:48 +0500

    Add

--- main-app/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ main-app/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c501e98d0882dc75b794501a1b220c26a501c27c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d90e7fef77adc22ce9f0bfa63fcb224316e2cc5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
