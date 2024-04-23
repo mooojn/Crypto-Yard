7c978625d75a52ec8e1ad87e544f1b5ecc33784e
mooojn <moontariq7867@gmail.com>
2024-04-23 21:18:43 +0500

    ignore debug files

--- main-app/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ main-app/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76efeff794202d5f8ed7462638529886e8e33797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2f039662338cc64b879334240f11862c68dc556")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
